741dee0abb48468917f8a2dc15e6cb4a39f7eb47
Casey Bee <caseye.baker@gmail.com>
2019-05-13 10:44:47 -0700

    queue implementation and runtime

--- queue/queue.py
+++ queue/queue.py
@@ -3,13 +3,20 @@ class Queue:
     self.size = 0
     # what data structure should we
     # use to store queue elements?
-    self.storage = 
+    self.storage = []
 
   def enqueue(self, item):
+    self.storage.append(item)
+    print(f"Enqueued")
     pass
   
   def dequeue(self):
+    if self.storage:
+        item = self.storage.pop(0)
+        print(f"Dequeued")
+        return item
     pass
 
   def len(self):
+    return len(self.storage)
     pass
