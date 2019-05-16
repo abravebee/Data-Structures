a16f8e68028edf78906d01b81d96aca2777a4556
Casey Bee <caseye.baker@gmail.com>
2019-05-16 13:27:35 -0700

    infinite loop happening during tests, I think because I hadn't written delete function yet; uncommented for now while I write sift down

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -13,6 +13,15 @@ class Heap:
     # removes and returns the 'topmost' value from the heap; 
     # this method needs to ensure that the heap property is maintained after the topmost element has been removed.
     def delete(self):
+        if len(self.storage) == 0:
+            return
+        elif len(self.storage) == 1:
+            return self.storage.pop()
+        
+        top = self.storage[0]
+        self.storage[0] = self.storage.pop()
+        self._sift_down(0)
+        return top
         pass
 
     #returns the maximum value in the heap 
@@ -31,20 +40,17 @@ class Heap:
     # if the parent's value is less than the value at the specified index.
     def _bubble_up(self, index):
         print(f"index: {index}, self.storage: {len(self.storage) - 1}")
-        if index < 1 or index > len(self.storage)-1:
+        if index <= 0 or index > len(self.storage)-1:
             print(f"{index} < 1 or {index} > {len(self.storage) - 1}")
-            pass
+            return
         parentIndex = (index-1) // 2
         if parentIndex > len(self.storage) or parentIndex < 0:
             return
-        while self.storage[index] and self.storage[parentIndex]:
-            while self.storage[index] > self.storage[parentIndex]:
-                hold = self.storage[index]
-                self.storage[index] = self.storage[parentIndex]
-                self.storage[parentIndex] = hold
-                index += 1
-                self._bubble_up(index)
-        return
+        if self.storage[index] > self.storage[parentIndex]:
+            hold = self.storage[index]
+            self.storage[index] = self.storage[parentIndex]
+            self.storage[parentIndex] = hold
+            self._bubble_up(parentIndex)
         pass
 
     # grabs the indices of this element's children 
